6a75a619880a8207e68b2cf4c5b5f12043977d6c
flynnbm <flynn.m.brian@gmail.com>
2025-10-29 17:49:49 -0400

    add main node for standalone node running of mtc plan and execute node which is written as a composable node

---
+++ compare_flexbe_utilities/src/mtc_plan_and_execute_pick_action_main.cpp
@@ -0,0 +1,18 @@
+#include <rclcpp/rclcpp.hpp>
+#include "mtc_plan_and_execute_pick_and_place_action.hpp"
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  auto opts = rclcpp::NodeOptions()
+                .allow_undeclared_parameters(true)
+                .automatically_declare_parameters_from_overrides(true);
+
+  auto node = std::make_shared<compare_flexbe_utilities::MtcPlanAndExecuteNode>(opts);
+
+  // Multi-threaded so action goal thread, feedback, and other callbacks don’t block each other
+  rclcpp::executors::MultiThreadedExecutor exec;
+  exec.add_node(node);
+  exec.spin();
+  rclcpp::shutdown();
+  return 0;
+}
